278360dae55fb88f7fa829212d021aaba1bcb64b
Your Name <rajatpandey542@gmail.com>
2026-03-16 21:52:26 +0530

    Display counter button

--- Habit Tracker/src/components/habit_item.jsx
+++ Habit Tracker/src/components/habit_item.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
+
 export const Habititem =({habit})=>{
+
+    const [counter, setcounter]=useState(0)
+
+    const click=()=>{
+        setcounter(counter+1)
+    }
     return(
 
         <div>
             <p>
                 {habit}
             </p>
+            <button onClick={click} >{counter}</button>
+
         </div>
     )
 }
