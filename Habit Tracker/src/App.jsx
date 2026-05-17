cea11a418797bf28b650063895a58b5231c35117
Your Name <rajatpandey542@gmail.com>
2026-05-17 13:30:58 +0530

    implement date, time functionality and completion date and time functionality in the array

--- Habit Tracker/src/App.jsx
+++ Habit Tracker/src/App.jsx
@@ -1,60 +1,61 @@
-import { useState, useEffect } from 'react'
-import { Input } from './components/input'
-import { Habititem } from './components/habit_item'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Input } from "./components/input";
+import { Habititem } from "./components/habit_item";
+import "./App.css";
 
 function App() {
-    const keyName = "habits"
+  const keyName = "habits";
+  const now = new Date();
+  
+  const [habit, setHabit] = useState("");
+  const [habitList, sethabitList] = useState(() => {
+    const storedHabit = localStorage.getItem(keyName);
+    return storedHabit ? JSON.parse(storedHabit) : [];
+  });
 
-    const [habit, setHabit] = useState("")
-    const [habitList, sethabitList] = useState(() => {
-        const storedHabit = localStorage.getItem(keyName)
-        return storedHabit ? JSON.parse(storedHabit) : []
-    })
-    const now =new Date();
-     
+  const Submit = () => {
+    if (!habit.trim()) return;
+    const newHabit = {
+      id: now.toLocaleString(),
+      title: habit,
+      counter: 0,
+      checked: false,
+      completionDate: [],
+    };
+    sethabitList([...habitList, newHabit]); //immutable state updates
+    setHabit("");
+  };
 
-    const Submit = () => {
-        if(!habit.trim()) return ;
-        const newHabit={
-            id:now.getTime(),
-            title:habit,
-            counter:0,
-            checked:false,
-            completionDate:[
-                now.toISOString()
-            ]
-        }
-        sethabitList([...habitList,newHabit]);//immutable state updates
-        setHabit("");
-    }
+  const updateHabit = (index, newValue) => {
+    const updatedhabits = [...habitList];
+    updatedhabits[index] = newValue;
+    sethabitList(updatedhabits);
+  };
+  const deleteHabit = (index) => {
+    const updatedList = habitList.filter((_, i) => i !== index);
+    sethabitList(updatedList);
+  };
 
-    const updateHabit = (index, newValue) => {
-        const updatedhabits = [...habitList]
-        updatedhabits[index] = newValue;
-        sethabitList(updatedhabits)
-        
-    }
-    const deleteHabit = (index) => {
-        const updatedList = habitList.filter((_, i) => i !== index)
-        sethabitList(updatedList)
-    }
+  useEffect(() => {
+    localStorage.setItem(keyName, JSON.stringify(habitList));
+  }, [keyName, habitList]);
 
-    useEffect(() => {
-        localStorage.setItem(keyName, JSON.stringify(habitList));
-    }, [keyName, habitList])
-
-    return (
-        <div>
-            <Input setHabit={setHabit} habit={habit} />
-            <button onClick={Submit}>Submit</button>
-            {habitList.map((h, index) => (
-                <Habititem key={h.id} habit={h} index={index} updateHabit={updateHabit} deleteHabit={deleteHabit} 
-                 />
-            ))}
-
-        </div>
-    )
+  return (
+    <div>
+      <Input setHabit={setHabit} habit={habit} />
+      <button onClick={Submit}>Submit</button>
+      {habitList.map((h, index) => (
+        <Habititem
+          key={h.id}
+          habit={h}
+          index={index}
+          updateHabit={updateHabit}
+          deleteHabit={deleteHabit}
+          now={now}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
