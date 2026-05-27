d85944cf7ccd18fbec2f945ef2ce9d9d6a060d7e
Your Name <rajatpandey542@gmail.com>
2026-05-27 20:08:31 +0530

    handle searchParams or query inside the path

--- Habit Tracker/server/index.js
+++ Habit Tracker/server/index.js
@@ -1,16 +1,20 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const url = require('url');
+
 const myServer = http.createServer((req, res)=>{
     const log =`${Date.now()}:${req.url} New Req recieved\n`
+    const myURL = new URL(req.url, `http://${req.headers.host}`);
+     console.log(myURL);
+    console.log("Pathname:", myURL.pathname);
    fs.appendFile("log.txt",log,(error, data)=>{
     console.log("Inside appendFile");
-
         if (error) {
             console.log(error);
             res.end("Error");
             return;
         }
-    switch(req.url){
+    switch(myURL.pathname){
         case "/":
             res.end("Hello from my Server");
             break;
@@ -18,10 +22,12 @@ const myServer = http.createServer((req, res)=>{
             res.end("This is my dashboard");
             break;
         case "/dashboard/under":
-            res.end("This is my under");
+            let username=myURL.searchParams.get("myname");
+            res.end(`hey, ${username} are you unmeployed`);
             break;
         case "/hashmap":
-            res.end("Hello from my hashmap");
+            let report=myURL.searchParams.get("myreport");
+            res.end(`yoo, take your ${report} to yourself`);
             break;
             default:
                 res.end("Page not found");
