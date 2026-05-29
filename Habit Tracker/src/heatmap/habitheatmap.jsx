3042a310b268a4b1b052de983effbb1a5682a1d3
Your Name <rajatpandey542@gmail.com>
2026-05-29 20:56:00 +0530

    solve the panelcolor bug  for the initialcolor

--- Habit Tracker/src/heatmap/habitheatmap.jsx
+++ Habit Tracker/src/heatmap/habitheatmap.jsx
@@ -1,36 +1,39 @@
 import React from "react";
-import Tooltip from '@uiw/react-tooltip';
+import Tooltip from "@uiw/react-tooltip";
 import HeatMap from "@uiw/react-heat-map";
-import { useState } from "react";
+import "./heatmap.css"
 
-export function HabitHeatMap({completionDate}) {
-  
-  const value = 
-   completionDate.map((date) => ({
-    date: date.replaceAll("-", "/"),
-    count: 1,
-  }));
+export function HabitHeatMap({ completionDate }) {
+  const value = completionDate
+    .filter((date) => date)
+    .map((date) => ({
+      date: date.replaceAll("-", "/"),
+      count: 1,
+    }));
   return (
-    <div>
-      
+    <div className="heatmap_container">
       <HeatMap
         value={value}
         width={700}
-        style={{ color: "#F5F5F5"}}
-         legendCellSize={0}
+        style={{ color: "#F5F5F5" }}
+        panelColors={{
+          0: "#FFF0E4", // inactive cells
+          1: "#306D29",
+        }}
+        legendCellSize={0}
         weekLabels={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
-        startDate={new Date("2026/01/01")}
+        startDate={new Date(`${new Date().getFullYear()}/01/01`)}
         legendRender={(props) => <rect {...props} y={props.y + 10} rx={5} />}
         rectProps={{
-          rx: 5
+          rx: 5,
         }}
         rectRender={(props, data) => {
-        return (
-          <Tooltip placement="top" content={`date: ${data.date}`}>
-            <rect {...props} />
-          </Tooltip>
-        );
-      }}
+          return (
+            <Tooltip placement="top" content={`date: ${data.date}`}>
+              <rect {...props} />
+            </Tooltip>
+          );
+        }}
       />
     </div>
   );
