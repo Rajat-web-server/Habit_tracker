685c5382862a500a5266e80c7551ca08e42f7245
Your Name <rajatpandey542@gmail.com>
2026-05-29 20:40:28 +0530

    Remove th legend below the heatmap

--- Habit Tracker/src/heatmap/habitheatmap.jsx
+++ Habit Tracker/src/heatmap/habitheatmap.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Tooltip from '@uiw/react-tooltip';
 import HeatMap from "@uiw/react-heat-map";
+import { useState } from "react";
 
 export function HabitHeatMap({completionDate}) {
   
@@ -8,20 +10,27 @@ export function HabitHeatMap({completionDate}) {
     date: date.replaceAll("-", "/"),
     count: 1,
   }));
-    
   return (
     <div>
-   
+      
       <HeatMap
         value={value}
         width={700}
         style={{ color: "#F5F5F5"}}
+         legendCellSize={0}
         weekLabels={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
         startDate={new Date("2026/01/01")}
         legendRender={(props) => <rect {...props} y={props.y + 10} rx={5} />}
         rectProps={{
           rx: 5
         }}
+        rectRender={(props, data) => {
+        return (
+          <Tooltip placement="top" content={`date: ${data.date}`}>
+            <rect {...props} />
+          </Tooltip>
+        );
+      }}
       />
     </div>
   );
