3bc2d3457e4d57521f17018efd57c504b351d64d
Your Name <rajatpandey542@gmail.com>
2026-06-06 15:38:26 +0530

    Provide a scrollable area to the analytics page

--- Habit Tracker/src/Pages/AnalyticsPage.jsx
+++ Habit Tracker/src/Pages/AnalyticsPage.jsx
@@ -1,17 +1,20 @@
 import { HabitHeatMap } from "../heatmap/habitheatmap";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 export const AnalyticsPage = ({ habitList }) => {
   return (
     <>
-      <div className="mt-25  ">
-        <div>
-          <div className="text-center pb-5 font-bold text-3xl">
+      <card className="h-screen flex flex-col mt-25  ">
+        
+          <CardHeader className="text-center pb-5 font-bold text-3xl shrink-0 border-b">
             The Analytics Page
-          </div>
-          <CardContent>
+          </CardHeader>
+
+          {/* scrollable area */}
+
+          <CardContent className="overflow-hidden flex-1 min-h-0 ">
             <ScrollArea className="h-full">
-              <div className="flex flex-col gap-4 ">
+              <div className="flex flex-col gap-4 pr-4 space-y-4 ">
                 {habitList.map((habit) => (
                   <Card
                     key={habit.id}
@@ -27,8 +30,8 @@ export const AnalyticsPage = ({ habitList }) => {
               </div>
             </ScrollArea>
           </CardContent>
-        </div>
-      </div>
+        
+      </card>
     </>
   );
 };
