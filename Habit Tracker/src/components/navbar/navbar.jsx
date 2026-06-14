1c655b332cc9cb608c5e1d5fe25efbc73d8dd5a4
Your Name <rajatpandey542@gmail.com>
2026-06-14 20:26:29 +0530

    Provide motion to the icons

--- Habit Tracker/src/components/navbar/navbar.jsx
+++ Habit Tracker/src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { motion } from "motion/react";
 
 export const Navbar = () => {
   const NavlinkStyles = ({ isActive }) => {
@@ -8,23 +9,69 @@ export const Navbar = () => {
     };
   };
   return (
-    <div >
+    <div>
       <nav className="flex items-center justify-center gap-6 text-textcolor1 fixed w-full top-0 p-6 bg-[#270c43]">
-        <NavLink style={NavlinkStyles} to="/">
-          <img width="20" height="20" src="https://img.icons8.com/material/24/FFFFFF/dashboard-layout.png" alt="dashboard-layout"/>
-        </NavLink>
+        <motion.div
+          whileHover={{ scale: 1.5 }}
+          whileTap={{ scale: 0.95 }}
+          onHoverStart={() => console.log("hover started!")}
+        >
+          <NavLink style={NavlinkStyles} to="/">
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/material/24/FFFFFF/dashboard-layout.png"
+              alt="dashboard-layout"
+            />
+          </NavLink>
+        </motion.div>
+
         <br />
-        <NavLink style={NavlinkStyles} className="" to="/habits">
-         <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/FFFFFF/task.png" alt="task"/>
-        </NavLink>
+        <motion.div
+          whileHover={{ scale: 1.5 }}
+          whileTap={{ scale: 0.95 }}
+          onHoverStart={() => console.log("hover started!")}
+        >
+          <NavLink style={NavlinkStyles} className="" to="/habits">
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/ios-filled/50/FFFFFF/task.png"
+              alt="task"
+            />
+          </NavLink>
+        </motion.div>
+
         <br />
-        <NavLink style={NavlinkStyles} to="/Analytics">
-        <img width="20" height="20" src="https://img.icons8.com/external-solidglyph-m-oki-orlando/32/FFFFFF/external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando.png" alt="external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando"/>
-        </NavLink>
-        <NavLink style={NavlinkStyles} to="/Chart">
-          <img width="20" height="20" src="https://img.icons8.com/liquid-glass/48/graph.png" alt="graph"/>
-        </NavLink>
-        
+        <motion.div
+          whileHover={{ scale: 1.5 }}
+          whileTap={{ scale: 0.95 }}
+          onHoverStart={() => console.log("hover started!")}
+        >
+          <NavLink style={NavlinkStyles} to="/Analytics">
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/external-solidglyph-m-oki-orlando/32/FFFFFF/external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando.png"
+              alt="external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando"
+            />
+          </NavLink>
+        </motion.div>
+        <br />
+        <motion.div
+          whileHover={{ scale: 1.5 }}
+          whileTap={{ scale: 0.95 }}
+          onHoverStart={() => console.log("hover started!")}
+        >
+          <NavLink style={NavlinkStyles} to="/Chart">
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/liquid-glass/48/graph.png"
+              alt="graph"
+            />
+          </NavLink>
+        </motion.div>
       </nav>
     </div>
   );
