6ebc849cff34e7437307b8771282d2a33f31c4a2
Your Name <rajatpandey542@gmail.com>
2026-06-14 19:17:17 +0530

    Provide alignement, contrast, icons to the navbar

--- Habit Tracker/src/components/navbar/navbar.jsx
+++ Habit Tracker/src/components/navbar/navbar.jsx
@@ -1,31 +1,30 @@
 import { NavLink } from "react-router-dom";
-import "./navbar.css";
 
 export const Navbar = () => {
-  const NavlinkStyles = ({isActive}) => {
+  const NavlinkStyles = ({ isActive }) => {
     return {
       fontWeight: isActive ? "bold" : "normal",
       textDecoration: isActive ? "none" : "underline",
     };
   };
   return (
-    <div className="navbar">
-      <nav>
+    <div >
+      <nav className="flex items-center justify-center gap-6 text-textcolor1 fixed w-full top-0 p-6 bg-[#270c43]">
         <NavLink style={NavlinkStyles} to="/">
-          Dashboard
+          <img width="20" height="20" src="https://img.icons8.com/material/24/FFFFFF/dashboard-layout.png" alt="dashboard-layout"/>
         </NavLink>
         <br />
-        <NavLink style={NavlinkStyles} to="/habits">
-          Habits
+        <NavLink style={NavlinkStyles} className="" to="/habits">
+         <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/FFFFFF/task.png" alt="task"/>
         </NavLink>
         <br />
         <NavLink style={NavlinkStyles} to="/Analytics">
-          Analytics
+        <img width="20" height="20" src="https://img.icons8.com/external-solidglyph-m-oki-orlando/32/FFFFFF/external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando.png" alt="external-heatmap-digital-marketing-solid-solidglyph-m-oki-orlando"/>
         </NavLink>
         <NavLink style={NavlinkStyles} to="/Chart">
-          Charts
+          <img width="20" height="20" src="https://img.icons8.com/liquid-glass/48/graph.png" alt="graph"/>
         </NavLink>
-        <br />
+        
       </nav>
     </div>
   );
