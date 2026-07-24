0b47927068909324f47ad8ea3c5f3149ecad10b6
Your Name <rajatpandey542@gmail.com>
2026-07-24 19:43:12 +0530

    add base inside your vite.config

--- Habit Tracker/vite.config.js
+++ Habit Tracker/vite.config.js
@@ -5,10 +5,11 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/habit_tracker/",
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}); 
+});
